327fc00cddd8825f2cccc8e8bbb33b305b06fb97
Vijay Gill <Vijay.Gill@hotmail.com>
2025-05-21 01:22:05 +0100

    Get latest version of runner from github itself.

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -4,6 +4,8 @@ UNAME=$(whoami)
 USERID=$(id -u)
 GRPID=$(cat /etc/group | grep docker| cut -d ":" -f3)
 
+RUNNER_URL=$(curl "https://github.com/actions/runner/releases/latest" -s -L -I -o /dev/null -w '%{url_effective}')
+RUNNER_VER="${RUNNER_URL##*/v}"
 
-docker build --build-arg=UNAME=${UNAME} --build-arg=UID=${USERID} --build-arg=GID=${GRPID} -t gh-actions-runner -f Dockerfile .
+docker build --build-arg=UNAME=${UNAME} --build-arg=UID=${USERID} --build-arg=GID=${GRPID} --build-arg=RUNNER_VER=${RUNNER_VER}  -t gh-actions-runner -f Dockerfile .
 
